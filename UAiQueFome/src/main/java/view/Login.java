81d38a1b13e9597a4c5ae036cb757e18adba6a43
WMrMW <marciofelipe.mfd@gmail.com>
2022-05-14 01:02:05 -0300

    Adicionando o prototipo

--- UAiQueFome/src/main/java/view/Login.java
+++ UAiQueFome/src/main/java/view/Login.java
@@ -25,6 +25,7 @@ public class Login extends javax.swing.JFrame {
         painel_do_titulo = new javax.swing.JPanel();
         tituloLB1 = new javax.swing.JLabel();
         tituloLB = new javax.swing.JLabel();
+        loginBTN1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("UAiQueFome");
@@ -36,6 +37,11 @@ public class Login extends javax.swing.JFrame {
         cadastro_RestauranteBTN.setBackground(new java.awt.Color(255, 125, 0));
         cadastro_RestauranteBTN.setForeground(new java.awt.Color(255, 255, 255));
         cadastro_RestauranteBTN.setText("Cadastrar Restaurante");
+        cadastro_RestauranteBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cadastro_RestauranteBTNActionPerformed(evt);
+            }
+        });
 
         loginLB.setBackground(new java.awt.Color(255, 125, 0));
         loginLB.setForeground(new java.awt.Color(255, 255, 255));
@@ -54,10 +60,20 @@ public class Login extends javax.swing.JFrame {
         cadastro_ClienteBTN.setBackground(new java.awt.Color(255, 125, 0));
         cadastro_ClienteBTN.setForeground(new java.awt.Color(255, 255, 255));
         cadastro_ClienteBTN.setText("Cadastrar Cliente");
+        cadastro_ClienteBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cadastro_ClienteBTNActionPerformed(evt);
+            }
+        });
 
         loginBTN.setBackground(new java.awt.Color(255, 125, 0));
         loginBTN.setForeground(new java.awt.Color(255, 255, 255));
-        loginBTN.setText("Entrar");
+        loginBTN.setText("Entrar Restaurante");
+        loginBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginBTNActionPerformed(evt);
+            }
+        });
 
         painel_do_titulo.setBackground(new java.awt.Color(249, 160, 63));
 
@@ -96,37 +112,52 @@ public class Login extends javax.swing.JFrame {
                 .addGap(23, 23, 23))
         );
 
+        loginBTN1.setBackground(new java.awt.Color(255, 125, 0));
+        loginBTN1.setForeground(new java.awt.Color(255, 255, 255));
+        loginBTN1.setText("Entrar Cliente");
+        loginBTN1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginBTN1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout painel_dos_dadosLayout = new javax.swing.GroupLayout(painel_dos_dados);
         painel_dos_dados.setLayout(painel_dos_dadosLayout);
         painel_dos_dadosLayout.setHorizontalGroup(
             painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(painel_dos_dadosLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(loginBTN, javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(painel_dos_dadosLayout.createSequentialGroup()
-                                .addComponent(senhaLB)
-                                .addGap(18, 18, 18)
-                                .addComponent(senhaPF))
-                            .addGroup(painel_dos_dadosLayout.createSequentialGroup()
-                                .addComponent(loginLB)
-                                .addGap(18, 18, 18)
-                                .addComponent(loginTF, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painel_dos_dadosLayout.createSequentialGroup()
-                            .addComponent(cadastro_ClienteBTN)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(cadastro_RestauranteBTN)))
-                    .addComponent(painel_do_titulo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(191, Short.MAX_VALUE)
+                .addGroup(painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(painel_dos_dadosLayout.createSequentialGroup()
+                        .addGroup(painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addGroup(painel_dos_dadosLayout.createSequentialGroup()
+                                        .addComponent(senhaLB)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(senhaPF))
+                                    .addGroup(painel_dos_dadosLayout.createSequentialGroup()
+                                        .addComponent(loginLB)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(loginTF, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painel_dos_dadosLayout.createSequentialGroup()
+                                    .addComponent(cadastro_ClienteBTN)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(cadastro_RestauranteBTN)))
+                            .addComponent(painel_do_titulo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(191, Short.MAX_VALUE))
+                    .addGroup(painel_dos_dadosLayout.createSequentialGroup()
+                        .addComponent(loginBTN)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(loginBTN1)
+                        .addGap(183, 183, 183))))
         );
         painel_dos_dadosLayout.setVerticalGroup(
             painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painel_dos_dadosLayout.createSequentialGroup()
                 .addGap(50, 50, 50)
                 .addComponent(painel_do_titulo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
                 .addGroup(painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(loginLB)
                     .addComponent(loginTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -135,7 +166,9 @@ public class Login extends javax.swing.JFrame {
                     .addComponent(senhaLB)
                     .addComponent(senhaPF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(loginBTN)
+                .addGroup(painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(loginBTN)
+                    .addComponent(loginBTN1))
                 .addGap(36, 36, 36)
                 .addGroup(painel_dos_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(cadastro_RestauranteBTN, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -157,6 +190,26 @@ public class Login extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void cadastro_RestauranteBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastro_RestauranteBTNActionPerformed
+        setVisible(false);
+        new CadastroRestaurante().setVisible(true);
+    }//GEN-LAST:event_cadastro_RestauranteBTNActionPerformed
+
+    private void loginBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBTNActionPerformed
+        setVisible(false);
+        new InicioRestaurante().setVisible(true);
+    }//GEN-LAST:event_loginBTNActionPerformed
+
+    private void loginBTN1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBTN1ActionPerformed
+        setVisible(false);
+        new InicioCliente().setVisible(true);
+    }//GEN-LAST:event_loginBTN1ActionPerformed
+
+    private void cadastro_ClienteBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastro_ClienteBTNActionPerformed
+        setVisible(false);
+        new CadastroCliente().setVisible(true);
+    }//GEN-LAST:event_cadastro_ClienteBTNActionPerformed
+
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -170,6 +223,7 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JButton cadastro_ClienteBTN;
     private javax.swing.JButton cadastro_RestauranteBTN;
     private javax.swing.JButton loginBTN;
+    private javax.swing.JButton loginBTN1;
     private javax.swing.JLabel loginLB;
     private javax.swing.JTextField loginTF;
     private javax.swing.JPanel painel_do_titulo;
